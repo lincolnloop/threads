05c0ab9833b19aa5179ebf467894a8d296218bd8
Marco Louro <mlouro@gmail.com>
2014-01-16 18:14:00 +0100

    animations structure

--- src/scripts/core/layoutManager.js
+++ src/scripts/core/layoutManager.js
@@ -44,7 +44,7 @@ module.exports = _.extend({
         $('body').removeClass('show-nav');
         // unmount existing component and mount new one
         this.activeTarget = target;
-        callback = _.partial(React.unmountComponentAtNode, this[target]);
+        callback = _.partial(React.unmountComponentAtNode, this.regions[prevTarget]);
         if (target === prevTarget) {
             callback();
         }
@@ -55,6 +55,28 @@ module.exports = _.extend({
         console.log('TeamListView:toggle');
         // TODO: Handle animations for the team list
         $('body').toggleClass('show-nav');
+    },
+    setupAnimation: function (prevRegion, nextRegion) {
+        var $prevRegion = $(prevRegion),
+            $nextRegion = $(nextRegion);
+
+        // make sure nextRegion width and height
+        // matches prevRegion
+        // (not sure this is really needed)
+        $nextRegion.width($prevRegion.width());
+        $nextRegion.height($prevRegion.height());
+    },
+    getAnimation: function () {
+
+    },
+    swipeLeft: function () {
+
+    },
+    swipeRight: function () {
+
+    },
+    reset: function () {
     }
+
 
 }, Backbone.Events);
