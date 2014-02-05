fe5198103b77b9e7fd0900e15eb1073a2aa93771
Marco Louro <mlouro@gmail.com>
2014-02-05 01:44:59 +0100

    2 spaces, get rid of window.app and require instead

--- src/apps/teams/router.js
+++ src/apps/teams/router.js
@@ -1,27 +1,28 @@
 "use strict";
 
 var Backbone = require('backbone'),
-    TeamDetailView = require('./views/TeamDetail.jsx'),
-    layoutManager = require('../../core/layoutManager'),
-    NavView = require('../../core/views/nav.jsx');
+  app = require('../../app'),
+  TeamDetailView = require('./views/TeamDetail.jsx'),
+  layoutManager = require('../../core/layoutManager'),
+  NavView = require('../../core/views/nav.jsx');
 
 var TeamRouter = Backbone.Router.extend({
-    routes: {
-        ":team/": 'detail'
-    },
-    detail: function (slug) {
-        // get current team from slug
-        var team = window.app.data.teams.findWhere({slug: slug});
-        // render team detail view
-        layoutManager.renderComponent(TeamDetailView({
-            team: team
-        }), 'contentMain');
-        // render top nav
-        layoutManager.renderComponent(NavView({
-            title: team.get('name'),
-            team: team
-        }), 'navMain');
-    }
+  routes: {
+    ":team/": 'detail'
+  },
+  detail: function (slug) {
+
+    var team = app.data.teams.findWhere({slug: slug});
+    // render team detail view
+    layoutManager.renderComponent(TeamDetailView({
+      team: team
+    }), 'contentMain');
+    // render top nav
+    layoutManager.renderComponent(NavView({
+      title: team.get('name'),
+      team: team
+    }), 'navMain');
+  }
 });
 
 module.exports = TeamRouter;
