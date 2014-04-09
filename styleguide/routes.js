bacb5890e3a5fc86e457d4aba0a1959d2e3fe155
Marco Louro <mlouro@gmail.com>
2014-04-09 12:11:57 +0200

    wip styleguide

---
+++ styleguide/routes.js
@@ -0,0 +1,15 @@
+'use strict';
+var config = require('../config/' + (process.env.NODE_ENV || 'development'));
+var pkg = require('../package.json');
+
+var production = (process.env.NODE_ENV === 'production');
+
+exports.index = function (req, res) {
+  console.log('routes:index');
+  res.cookie('config', JSON.stringify(config));
+  res.render('index', {
+    app: '/' + pkg.name + (production ? '.min' : '') + '.js',
+    vendor: '/vendor' + (production ? '.min' : '') + '.js',
+    css: '/' + pkg.name + (production ? '.min' : '') + '.css',
+  });
+};
