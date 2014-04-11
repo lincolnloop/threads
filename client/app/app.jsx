0af6bfa5234d83c04d0a1639710903d4623527dd
Marco Louro <mlouro@gmail.com>
2014-04-11 11:13:34 +0200

    refactor routing, take controller-views off of app.jsx

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -5,7 +5,6 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 var React = require('react');
 var router = require('./router');
-var urls = require('./urls');
 var store = require('./store');
 var log = require('loglevel');
 var CSSTransitionGroup = require('react/lib/ReactCSSTransitionGroup');
@@ -16,45 +15,60 @@ var CSSTransitionGroup = require('react/lib/ReactCSSTransitionGroup');
 // common
 var TopNav = require('./components/TopNav.jsx');
 
-// auth
-var SignInView = require('./auth/sign-in.jsx');
-
-// team views
-var OrganizationList = require('./teams/organization-list.jsx');
-var TeamDetailView = require('./teams/detail.jsx');
-var teamUtils = require('./teams/utils');
-
-// discussion views
-var DiscussionDetailView = require('./discussions/detail.jsx');
-var DiscussionCreateView = require('./discussions/create.jsx');
+// --------------------
+// routes
+// --------------------
+var authRoutes = require('./auth/routes');
+var teamRoutes = require('./teams/routes');
+var discussionRoutes = require('./discussions/routes');
 
 var AppView = React.createClass({
+  
+  route: function(view) {
+    // shortcut for route callbacks
+    return _.partial(this.updateUI, view);
+  },
 
-  updateUI: function(options) {
-    // helper method to handle which css transitions should ocurr
-    // and trigger setState to re-render the UI
+  updateUI: function() {
+    // Page transition helper method.
+    // Determines which animation should ocurr for page changes
+    // and triggers setState to re-render the UI
+    // - arguments > []
+    //   - view
+    //   - *route arguments
+
+    // The first argument on *arguments* is always the view
+    // so extract it from the *arguments* list
+    // and invoke the view with the remaining arguments
+    // that come from the url router (e.g. teamSlug, discussionId, etc...)
+    var options = {};
+    var view = Array.prototype.shift.apply(arguments);
+    log.debug('app:updateUI', view, arguments);
+    try {
+      // each controller-view should return a list of React views
+      // and the `navLevel` of the view as an object.
+      options = view.apply(null, arguments);
+    } catch(e) {
+      log.error(e);
+    }
+    // Animation is calculated based on the current and next values of 
+    // *navLevel* (`this.state.navLevel` and `options.navLevel` respectively)
+    // If the next `navLevel` is higher than the current `navLevel`, 
+    // it means we're going deeper into the navigation, so we need to simulate
+    // a forward motion (new content comes from the right to the left).
     var transition = options.navLevel > this.state.navLevel ? 'right-to-left' : 'left-to-right';
     var content = CSSTransitionGroup({
       'transitionName': transition,
       'component': React.DOM.div,
       'children': options.content
     });
+    // update state of our content and nav views
+    // and also store the current navLevel.
     this.setState({
       'content': content,
       'topNav': options.topNav,
       'navLevel': options.navLevel
     });
-  },
-
-  render: function() {
-    return (
-      <div className="main">
-        <nav id="top-nav">{this.state.topNav}</nav>
-        <div id="content">
-          {this.state.content}
-        </div>
-      </div>
-    );
   },
 
   startSuccess: function() {
@@ -73,14 +87,11 @@ var AppView = React.createClass({
 
   getInitialState: function() {
     return {
-      'topNav': TopNav({
-        'title': 'Threads'
-      }),
+      'topNav': '',
       'content': '',
-      // TODO: Collection and User instances should not
-      // be stored in state.
+      'navLevel': 0,
       'user': {},
-      'teams': []
+      'teams': [],
     };
   },
 
@@ -88,12 +99,23 @@ var AppView = React.createClass({
     //
     // Route view binding
     //
-    router.on('route:index', this.index);
-    router.on('route:signIn', this.signIn);
-    router.on('route:signOut', this.signOut);
-    router.on('route:team:detail', this.teamDetail);
-    router.on('route:team:create', this.discussionCreate);
-    router.on('route:discussion:detail', this.discussionDetail);
+    router.on('route:index', this.route(teamRoutes.list));
+    router.on('route:signIn', this.route(authRoutes.signIn));
+    router.on('route:signOut', this.route(authRoutes.signIn));
+    router.on('route:team:detail', this.route(teamRoutes.detail));
+    router.on('route:team:create', this.route(discussionRoutes.create));
+    router.on('route:discussion:detail', this.route(discussionRoutes.detail));
+  },
+
+  render: function() {
+    return (
+      <div className="main">
+        <nav id="top-nav">{this.state.topNav}</nav>
+        <div id="content">
+          {this.state.content}
+        </div>
+      </div>
+    );
   },
 
   componentDidMount: function() {
@@ -102,116 +124,6 @@ var AppView = React.createClass({
     window.addEventListener('store:fetchSuccess', this.startSuccess);
     window.addEventListener('store:fetchFailed', this.startFailed);
     store.fetch();
-  },
-
-  // pages
-  index: function() {
-    var teams = store.findAll('teams');
-    var organizations = teamUtils.groupByOrganizations(teams);
-    var contentView = '';
-
-    if (organizations && organizations.length) {
-      contentView = OrganizationList({
-        organizations: organizations
-      });
-    }
-    var navView = TopNav({
-      'title': 'Threads'
-    });
-
-    this.updateUI({
-      'content': contentView,
-      'topNav': navView,
-      'navLevel': 0
-    });
-  },
-
-  signIn: function() {
-    // content > sign in view
-    var contentView = SignInView({
-      'success': store.fetch.bind(store)
-    });
-    var navView = TopNav({
-      'title': 'Sign In'
-    });
-
-    this.updateUI({
-      'content': contentView,
-      'topNav': navView,
-      'navLevel': 0
-    });
-  },
-
-  signOut: function() {
-    log.debug('main:signOut');
-  },
-
-  teamDetail: function(teamSlug) {
-    log.debug('team:detail');
-    var navLevel = 1;
-    var team = store.find('teams', {slug: teamSlug});
-    // content > team discussion list view
-    // TODO: We need to bootstrap teams on load
-    var contentView = TeamDetailView({
-      'team': team,
-      'key': teamSlug
-    });
-    var navView = TopNav({
-      'title': team.name,
-      'backLink': '/'
-    });
-
-    this.updateUI({
-      'content': contentView,
-      'topNav': navView,
-      'navLevel': 5
-    });
-  },
-
-  discussionCreate: function (teamSlug) {
-    log.debug('DiscussionRouter:create');
-    var team = store.find('teams', {slug: teamSlug});
-    // content > create view
-    var contentView = DiscussionCreateView({
-      'team': team.url,
-      'key': 'create-' + team.slug
-    });
-    var navView = TopNav({
-      'title': team.name,
-      'team': team,
-      'backLink': urls.get('team:detail', {'slug': teamSlug})
-    });
-
-    this.updateUI({
-      'content': contentView,
-      'topNav': navView,
-      'navLevel': 10
-    });
-  },
-
-  discussionDetail: function(teamSlug, discussionId) {
-    log.debug('discussion:detail');
-    var team = store.find('teams', {slug: teamSlug});
-    var discussionUrl = urls.get('api:discussionChange', {
-      'discussion_id': discussionId
-    });
-    // content > discussion detail view
-    var contentView = DiscussionDetailView({
-      'team': team,
-      'discussion': discussionUrl,
-      'key': 'discussion-detail' + discussionUrl
-    });
-    var navView = TopNav({
-      'title': team.name,
-      'team': team,
-      'backLink': urls.get('team:detail', {'slug': teamSlug})
-    });
-
-    this.updateUI({
-      'content': contentView,
-      'topNav': navView,
-      'navLevel': 15
-    });
   }
 });
 
---
+++ client/app/discussions/routes.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var store = require('../store');
+var urls = require('../urls');
+var TopNav = require('../components/TopNav.jsx');
+var DiscussionDetailView = require('./detail.jsx');
+var DiscussionCreateView = require('./create.jsx');
+
+var routes = {
+  'create': function(teamSlug) {
+    var team = store.find('teams', {'slug': teamSlug});
+    // content > create view
+    var contentView = DiscussionCreateView({
+      'team': team.url,
+      'key': 'create-' + team.slug
+    });
+    var navView = TopNav({
+      'title': team.name,
+      'team': team,
+      'backLink': urls.get('team:detail', {'slug': teamSlug})
+    });
+
+    return {
+      'content': contentView,
+      'topNav': navView,
+      'navLevel': 10
+    };
+  },
+
+  'detail': function(teamSlug, discussionId) {
+    var team = store.find('teams', {'slug': teamSlug});
+    var discussionUrl = urls.get('api:discussionChange', {
+      'discussion_id': discussionId
+    });
+    // content > discussion detail view
+    var contentView = DiscussionDetailView({
+      'team': team,
+      'discussion': discussionUrl,
+      'key': 'discussion-detail' + discussionUrl
+    });
+    var navView = TopNav({
+      'title': team.name,
+      'team': team,
+      'backLink': urls.get('team:detail', {'slug': teamSlug})
+    });
+
+    return {
+      'content': contentView,
+      'topNav': navView,
+      'navLevel': 15
+    };
+  }
+};
+
+module.exports = routes;
