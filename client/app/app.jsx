773b4a96fc72c8e1d0d5ea2738f9d53a037f8e8c
Marco Louro <mlouro@gmail.com>
2014-04-24 11:30:28 +0200

    fixed background for list items

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -100,7 +100,7 @@ var AppView = React.createClass({
   },
 
   startFailed: function(error) {
-    log.error('startFailed', error);
+    log.error('start:failed', error);
     // Initial data fetch failed.
     // For now, we just assume sign in error.
     // redirect to sign-in page
