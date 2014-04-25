e38d17052da97c2f93f796af6089bb34a6553056
Marco Louro <mlouro@gmail.com>
2014-04-25 12:57:33 +0200

    hot fix for static files

--- gulp/static.js
+++ gulp/static.js
@@ -1,12 +1,15 @@
 'use strict';
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 
 gulp.task('static', function(){
   var production = (process.env.NODE_ENV === 'production');
 
-  var sources = ['client/static', 'node_modules/font-awesome/fonts'];
+  // Copy static files to the build directory
+  gulp.src('client/static/**/*')
+    .pipe(gulp.dest(production ? 'dist/static/' : 'build/static/'));
 
   // Copy static files to the build directory
-  return gulp.src(sources)
-    .pipe(gulp.dest(production ? 'dist/' : 'build/'));
+  return gulp.src('node_modules/font-awesome/fonts/**/*')
+    .pipe(gulp.dest(production ? 'dist/fonts/' : 'build/fonts/'));
 });
