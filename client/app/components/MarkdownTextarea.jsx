345a826112df1d65ee566c9384d121fa37bdd282
Marco Louro <mlouro@gmail.com>
2014-05-06 22:00:15 +0200

    markdown preview redesign wip refs #35

--- client/app/components/MarkdownTextarea.jsx
+++ client/app/components/MarkdownTextarea.jsx
@@ -9,59 +9,13 @@ var urls = require('../urls');
 var config = require('../utils/config');
 
 var MarkdownView = React.createClass({
-  getInitialState: function () {
-    return {
-      'previewValue': null,
-      'rawValue': this.props.defaultValue ? this.props.defaultValue : null
-    };
-  },
-  render: function () {
-    // render preview and textarea separately.
-    var defaultValue = this.props.data && this.props.data.raw_body,
-      nodes = [];
-    if (this.state.previewValue) {
-      // if we have a previewValue show a preview
-      // -----------
-      // Preview div
-      nodes.push(
-        React.DOM.div({
-          'className': 'preview-content',
-          'dangerouslySetInnerHTML': {'__html': this.state.previewValue}
-        }),
-        // Preview button
-        React.DOM.a({
-          'className': 'preview preview-stop',
-          'children': 'Close Preview',
-          'onClick': this.stopPreview
-        })
-      )
-    } else {
-      // Otherwise show the textarea
-      nodes.push(
-        // Textarea
-        React.DOM.textarea({
-          'ref': 'textarea',
-          'placeholder': this.props.placeholder,
-          'defaultValue': this.state.rawValue,
-          'required': !!this.props.required
-        }),
-        // Stop preview button
-        React.DOM.a({
-          'className': 'preview preview-start',
-          'children': 'Preview',
-          'onClick': this.preview
-        })
-      )
-    }
-    return (
-      React.DOM.div({'className': 'markdown-textarea'}, nodes)
-    )
-  },
-  getRawValue: function () {
+
+  getRawValue: function() {
     // Get raw value for the textarea
     return this.refs.textarea.getDOMNode().value.trim();
   },
-  preview: function () {
+
+  preview: function() {
     // Fetch the preview from the server
     $.ajax({
       'type': 'POST',
@@ -78,8 +32,43 @@ var MarkdownView = React.createClass({
       }.bind(this)
     });
   },
-  stopPreview: function () {
+
+  stopPreview: function() {
     this.setState({'previewValue': null});
+  },
+
+  getInitialState: function() {
+    return {
+      'previewValue': null,
+      'rawValue': this.props.defaultValue ? this.props.defaultValue : null
+    };
+  },
+  render: function() {
+    // render preview and textarea separately.
+    var defaultValue = this.props.data && this.props.data.raw_body;
+    return (
+      <div className="markdown-textarea">
+        <ul className="accordion-tabs">
+          <li className="tab-header-and-content">
+            <a onClick={this.stopPreview} className="tab-link is-active">Write</a>
+            <section className="is-open">
+              <textarea ref="textarea" 
+                        placeholder={this.props.placeholder}
+                        defaultValue={this.state.rawValue}
+                        required={!!this.props.required} />
+            </section>
+          </li>
+          <li className="tab-header-and-content">
+            <a onClick={this.preview} className="tab-link">Preview</a>
+            <section className="is-open">
+              <div className="preview-content"
+                   dangerouslySetInnerHTML={{'__html': this.state.previewValue}}>
+              </div>
+            </section>
+          </li>
+        </ul>
+      </div>
+    )
   }
 });
 
--- client/app/discussions/DiscussionCreate.jsx
+++ client/app/discussions/DiscussionCreate.jsx
@@ -8,23 +8,7 @@ var urls = require('../urls');
 
 var DiscussionCreateView = React.createClass({
 
-  render: function () {
-    return (
-      <form className="discussion-create form-view" onSubmit={this.submit}>
-        <div className="form-view-actions">
-          <a href={this.props.cancelLink} className="btn btn-cancel ">Cancel</a>
-          <button type="button" className="btn btn-preview">Preview</button>
-          <button type="submit" className="btn btn-submit">Create</button>
-        </div>
-        <div className="form-view-fields">
-          <input type="text" placeholder="What are we talking about?" ref="title" required />
-          <MarkdownView placeholder="Comment.." ref="comment" required />
-        </div>
-      </form>
-    );
-  },
-
-  submit: function (evt) {
+  handleSubmit: function(evt) {
     var title = this.refs.title.getDOMNode().value;
     store.add('discussions', {
       'title': title,
@@ -43,6 +27,27 @@ var DiscussionCreateView = React.createClass({
       router.navigate(url, {'trigger': true});
     });
     return false;
+  },
+
+  getInitialState: function() {
+    return {
+      'previewing': false
+    }
+  },
+
+  render: function() {
+    return (
+      <form className="discussion-create form-view" onSubmit={this.handleSubmit}>
+        <div className="form-view-actions">
+          <a href={this.props.cancelLink} className="btn btn-cancel ">Cancel</a>
+          <button type="submit" className="btn btn-submit">Create</button>
+        </div>
+        <div className="form-view-fields">
+          <input type="text" placeholder="What are we talking about?" ref="title" required />
+          <MarkdownView placeholder="Comment.." ref="comment" required />
+        </div>
+      </form>
+    );
   }
 
 });
