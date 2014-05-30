77ac66b2e0a8348f0d9d40f279b4d3239132cb18
Marco Louro <mlouro@gmail.com>
2014-05-30 16:19:26 +0200

    load more search

--- client/app/search/Search.jsx
+++ client/app/search/Search.jsx
@@ -10,36 +10,64 @@ var store = require('../store');
 var SearchResult = require('./SearchResult.jsx');
 
 var SearchView = React.createClass({
-  handleSubmit: function() {
-    var team = this.refs.team.getDOMNode().value;
-    if (this.state.loading === true) {
-      return false;
-    }
-    this.setState({
-      'loading': true,
-      'count': null,
-      'results': null,
-      'next': null,
-      'team': null
-    });
+  fetch: function(query, team, page) {
     // https://gingerhq.com/api/v2/searchresult/?q=test&team=&user=&start_date=&end_date=
-    $.ajax(config.apiUrl + '/api/v2/searchresult/', {
+    return $.ajax(config.apiUrl + '/api/v2/searchresult/', {
       'dataType': 'json',
       'data': {
-        'q': this.refs.query.getDOMNode().value,
-        'team': team
+        'q': query,
+        'team': team,
+        'page': page
       },
       'headers': {
         'Authorization': 'Token ' + localStorage.apiKey
       }
-    }).done(function(response) {
+    });
+  },
+  handleLoadMore: function() {
+    // don't allow a new query if still loading
+    if (this.state.loading === true) {
+      return false;
+    }
+    this.setState({'loading': true});
+
+    this.fetch(this.state.query, this.state.team, this.state.next).done(function(response) {
+      // merge results
+      //var results = 
       this.setState({
         'loading': false,
         'count': response.count,
+        'results': _.union(this.state.results, response.results),
+        'next': response.next,
+        'page': response.page
+      });
+    }.bind(this));
+  },
+  handleSubmit: function() {
+    var query = this.refs.query.getDOMNode().value;
+    var team = this.refs.team.getDOMNode().value;
+    var page = 1;
+    // reset state
+    this.setState({
+      'loading': true,
+      'query': null,
+      'count': null,
+      'results': null,
+      'page': null,
+      'next': null,
+      'team': null
+    });
+    // fetch results
+    this.fetch(query, team, page).done(function(response) {
+      this.setState({
+        'loading': false,
+        'query': query,
+        'count': response.count,
         'results': response.results,
         'next': response.next,
-        'team': team
-      })
+        'team': team,
+        'page': response.page
+      });
     }.bind(this));
     return false;
   },
@@ -47,8 +75,10 @@ var SearchView = React.createClass({
   getInitialState: function() {
     return {
       'loading': false,
+      'query': null,
       'count': null,
       'results': null,
+      'page': null,
       'next': null,
       'team': null
     }
@@ -61,7 +91,7 @@ var SearchView = React.createClass({
         <div className="form-view-fields">
           <input type="text" ref="query" className="query" placeholder="Search..." required />
           <select ref="team" className="team">
-            <option>Select a team...</option>
+            <option value="">Select a team...</option>
             {_.map(teams, function(team) {
               return (<option value={team.name}>{team.name}</option>)
             })}
@@ -73,6 +103,9 @@ var SearchView = React.createClass({
           <input type="date" className="date" placeholder="End date" />
           <button type="submit" className="btn btn-search">{this.state.loading ? 'Searching...' : 'Search'}</button>
         </div>
+        {this.state.results !== null ?
+          <h4>{this.state.count} results found for "{this.state.query}"</h4>
+        : null}
         {this.state.results !== null ? <div className="search-list">
           {_.map(this.state.results, function(result, key) {
             return (<SearchResult key={'result-' + key} result={result} />);
@@ -80,6 +113,9 @@ var SearchView = React.createClass({
         </div> : this.state.results == 0 ? <div className="search-list empty">
           <h2>No results</h2>
         </div> : null}
+        {this.state.next ? 
+          <a onClick={this.handleLoadMore} className="load-more">{this.state.loading ? 'Loading...' : 'Load more...'}</a>
+        : null}
       </form>
     );
   }
