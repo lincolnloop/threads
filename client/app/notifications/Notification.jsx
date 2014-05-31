cb0c754426b6b3d7a917c519c23195c1af8d66b0
Marco Louro <mlouro@gmail.com>
2014-05-31 19:34:44 +0200

    split notifications into two components refs #44

---
+++ client/app/notifications/Notification.jsx
@@ -0,0 +1,45 @@
+'use strict';
+
+var $ = require('jquery');
+var _ = require('underscore');
+var classSet = require('react/lib/cx');
+var log = require('loglevel');
+var moment = require('moment');
+var React = require('react');
+var urls = require('../urls');
+var store = require('../store');
+var gravatar = require('../utils/gravatar');
+
+var NotificationView = React.createClass({
+
+  render: function() {
+    var notification = this.props.notification;
+    var user = store.find('users', notification.from_user);
+    var avatar = gravatar.get(user.email);
+    var notificationType = notification.verb.substring(0, 7);
+
+    var classList = {
+      'notification-item': true,
+      'unread': !notification.is_read
+    }
+    classList[notificationType] = true;
+
+    return (
+      <li className={classSet(classList)}>
+      <a href={notification.link}><div className="avatar" ><img src={avatar} /></div>
+      <div className="notification-content">
+        <span className="username">{user.name} </span>
+        <span className="user-action">{notification.verb} </span>
+        <span className="notification-title">{notification.title} </span>
+        <div className="notification-meta" >
+          <span className="notification-type"></span>
+          <span className="date">{moment(notification.date_created).fromNow()}.</span>
+        </div>
+      </div></a>
+    </li>
+    );
+  }
+
+});
+
+module.exports = NotificationView;
--- client/app/notifications/NotificationList.jsx
+++ client/app/notifications/NotificationList.jsx
@@ -9,6 +9,7 @@ var React = require('react');
 var urls = require('../urls');
 var store = require('../store');
 var gravatar = require('../utils/gravatar');
+var Notification = require('./Notification.jsx');
 
 var NotificationListView = React.createClass({
 
@@ -19,29 +20,9 @@ var NotificationListView = React.createClass({
       <div className="notifications">
         <ul className="notifications-list content-view">
           {_.map(notifications, function(notification, key) {
-            var user = store.find('users', notification.from_user);
-            var avatar = gravatar.get(user.email);
-            var notificationType = notification.verb.substring(0, 7);
-
-            var classList = {
-              'notification-item': true,
-              'unread': !notification.is_read
-            }
-            classList[notificationType] = true;
-
-            return (<li className={classSet(classList)} key={'n-' + key}>
-              <a href={notification.link}><div className="avatar" ><img src={avatar} /></div>
-              <div className="notification-content">
-                <span className="username">{user.name} </span>
-                <span className="user-action">{notification.verb} </span>
-                <span className="notification-title">{notification.title} </span>
-                <div className="notification-meta" >
-                  <span className="notification-type"></span>
-                  <span className="date">{moment(notification.date_created).fromNow()}.</span>
-                </div>
-              </div></a>
-            </li>)
-          }.bind(this))}
+            return <Notification key={'n-' + key}
+                                 notification={notification} />
+          })}
         </ul>
       </div>
     );
