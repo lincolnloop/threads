7201f5bff1b61cf15f7830f6cf8867b54c34b61a
Marco Louro <mlouro@gmail.com>
2014-06-03 18:32:43 +0200

    loading status indicator on the header refs #63

---
+++ client/app/mixins/eventsMixin.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var log = require('loglevel');
+var ee = require('event-emitter');
+var emitter = ee({});
+
+var eventsMixin = {
+
+  'emitter': emitter,
+
+  componentWillUnmount: function() {
+    // TODO: clear events binded by this component
+  }
+};
+
+module.exports = eventsMixin;
