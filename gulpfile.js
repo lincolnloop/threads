94a68b2dc96df72ae5cfa0ef11ca8ad8a4f77046
Marco Louro <mlouro@gmail.com>
2014-06-06 19:18:42 +0200

    wip new gulp deployments

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,35 @@
 'use strict';
-var browserify = require('browserify');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var livereload = require('gulp-livereload');
+var watchify = require('watchify');
+var rename = require('gulp-rename');
+// javascript
+var browserify = require('browserify');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
-var livereload = require('gulp-livereload');
-var source = require('vinyl-source-stream');
-var handlebars = require('gulp-compile-handlebars');
-var watchify = require('watchify');
-
 var bundle = require('./gulp/bundle');
-
-// live reload server
-var reloadServer = livereload();
+// sass
+var neat = require('node-neat').includePaths;
+var sass = require('gulp-sass');
+// html
+var handlebars = require('gulp-compile-handlebars');
 
 // File sources
 var sources = {
-  'js': './client/index.js',
+  'browserify': './client/index.js',
   'jshint': [
     'gulpfile.js',
     'gulp/**/*.js',
     'client/index.js',
     'client/app/**/*.js'
-  ]
+  ],
+  'css': [
+    'client/sass/**/*.css',
+    'client/sass/**/*.scss'
+  ],
+  'sass': 'client/sass/app.scss',
+  'html': 'server/views/index.hbs'
 };
 
 // --------------------------
@@ -31,11 +38,8 @@ var sources = {
 require('./gulp/clean');
 require('./gulp/static');
 require('./gulp/help');
-require('./gulp/sass');
-require('./gulp/html');
 require('./gulp/serve');
 require('./gulp/tests');
-require('./gulp/watch');
 
 // --------------------------
 // JavaScript dev tasks
@@ -46,55 +50,95 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter(stylish));
 });
 
-// watch JS for changes and livereload
-gulp.task('watch:js', function() {
-  var bundler = watchify(sources.js);
-  var opts = {
+
+// --------------------------
+// Development
+// --------------------------
+gulp.task('dev', function() {
+
+  // create live reload server
+  var reloadServer = livereload();
+
+  //
+  // js bundle
+  //
+  var jsbundler = watchify(sources.browserify);
+  var jsopts = {
     'debug': true,
     'name': 'threads.js',
     'dest': 'build/'
   };
-
-  bundler.on('update', function(evt) {
+  // watch js
+  jsbundler.on('update', function(evt) {
     gutil.log(gutil.colors.bgGreen('Reloading scripts...'));
-    gulp.run('jshint', 'tests');
     reloadServer.changed(evt[0]);
-    return bundle(bundler, opts);
+    return bundle(jsbundler, jsopts);
   });
-
+  // jshint
   gulp.watch(sources.jshint, ['jshint']);
 
-  return bundle(bundler, opts);
+  //
+  // css bundle
+  //
+  var sass = gulp.watch(sources.css, ['sass']);
+  sass.on('change', function(event) {
+    gutil.log(gutil.colors.bgGreen('Reloading sass...'));
+    reloadServer.changed(event.path);
+  });
+
+  // run once
+  // js
+  bundle(jsbundler, jsopts);
+
+  gutil.log(gutil.colors.bgGreen('Watching for changes...'));
 });
 
 // --------------------------
-// Deployment tasks
+// Deployment
 // --------------------------
 gulp.task('dist', function() {
-  var timestamp = +new Date;
-  gutil.log(timestamp);
+  // timestamp to version files
+  var timestamp = +new Date();
+  // destination folder
   var dest = 'dist/';
+  // versioned filenames
   var filenames = {
-    'js': 'threads.min.js',
-    'sass': 'threads.min.scss'
-  }
+    'js': 'threads.'+ timestamp +'.js',
+    'sass': 'threads.'+ timestamp +'.scss',
+    'html': 'index.html'
+  };
 
+  //
   // javascripts
+  //
   var bundler = browserify(sources.js);
-  return bundle(bundler, {
+  bundle(bundler, {
     'uglify': false, // TODO: uglify is broken
     'name': filenames.js,
     'dest': dest
   });
 
+  //
   // sass
-
-  // generate html
-  gulp.src('server/views/index.hbs')
+  //
+  gulp.src(sources.sass)
+    .pipe(sass({
+      'includePaths': neat,
+      'outputStyle': 'compressed'
+    }))
+    // error logging
+    .on('error', gutil.log)
+    // give it a file and save
+    .pipe(rename(filenames.sass))
+    .pipe(gulp.dest(dest));
+  //
+  // index.html
+  //
+  gulp.src(sources.html)
     // parse through handlebars templates
-    .pipe(handlebars(data))
+    .pipe(handlebars(filenames))
     // save as index.html to the dist directory
-    .pipe(source('index.html'))
+    .pipe(rename(filenames.html))
     .pipe(gulp.dest(dest));
 });
 
@@ -104,12 +148,9 @@ gulp.task('dist', function() {
 // build task
 gulp.task('build', [
   'jshint',
-  'clean',
-  'watch:js',
   'tests',
-  'sass',
-  'static',
-  'html'
+  'clean',
+  'static'
 ]);
 
 gulp.task('default', ['build'], function() {
