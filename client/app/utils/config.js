1e74dda1c7972394810b48664adafe26b3b19667
Marco Louro <mlouro@gmail.com>
2014-07-01 20:50:58 +0200

    default api url points to usethreads.com

--- client/app/utils/config.js
+++ client/app/utils/config.js
@@ -4,5 +4,5 @@ var clientconfig = require('clientconfig');
 // The default config can be overridden/extended by config passed in by cookie
 module.exports = _.extend({
   debug: false,
-  apiUrl: 'https://gingerhq.com'
+  apiUrl: 'https://usethreads.com'
 }, clientconfig);
