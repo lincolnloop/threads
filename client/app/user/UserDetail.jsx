7ddc5a59d8f15a895288fba81361f7480f3e5a76
Marco Louro <mlouro@gmail.com>
2014-08-27 17:42:08 +0200

    user profile view tweaks

--- client/app/user/UserDetail.jsx
+++ client/app/user/UserDetail.jsx
@@ -1,14 +1,17 @@
 'use strict';
 
 var React = require('react');
+var gravatar = require('../utils/gravatar');
 
 var UserDetail = React.createClass({
 
   render: function() {
+    var user = this.props.user;
 
     return (<div className="content-view">
-        <h2>{this.props.user.name}</h2>
-        <p>{this.props.user.email}</p>
+        <img src={gravatar.get(user.email)} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
       </div>)
   }
 });
