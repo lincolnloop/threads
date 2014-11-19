f4629b788f638d82b8ab8dd4c6d8eba7b5b09ff6
Marco Louro <mlouro@gmail.com>
2014-11-19 18:34:58 +0100

    removed static message info on message reply, updated dependencies

--- client/app/messages/MessageReply.jsx
+++ client/app/messages/MessageReply.jsx
@@ -28,10 +28,6 @@ var MessageReplyView = React.createClass({
     return false;
   },
 
-  toggleExpand: function() {
-    this.setState({'expand': !this.state.expand});
-  },
-
   getInitialState: function() {
     return {
       'expand': true
@@ -43,29 +39,23 @@ var MessageReplyView = React.createClass({
     var team = store.find('teams', {'slug': kwargs.team_slug});
     var message = store.find('messages', {'id': parseInt(kwargs.message_id)});
     var author = message ? store.find('users', message.user) : null;
+    log.info(message ? message.body : null);
     return (
       <div className="message-reply content-view">
         <form className="form-view" onSubmit={this.handleSubmit}>
           {message ? <div className="message">
 
-            <div className="message-container" onClick={this.toggleExpand}>
+            <div className="message-container">
 
             <div className="message-header">
-              <a className="collapse-button expand">{!this.state.expand ? "Expand" : "Collapse"}</a>
               <div className="avatar">
                 <img src={gravatar.get(author.email)} />
               </div>
               <div className="username">{author.name}</div>
-              <div className="date">
-                <a href="/lincoln-loop/12461/potential-project-united-nations-world-food-programme-wfp/#89624" 
-                   className="permalink">
-                  <time className="timeago" datetime="2014-02-06T22:02:23.791">{moment(message.date_created).fromNow()}</time>
-                </a>
-              </div>
             </div>
-              {this.state.expand ? <div className="message-content">
+              <div className="message-content">
                 <div dangerouslySetInnerHTML={{__html: message.body}} />
-              </div> : null}
+              </div>
             </div>
           </div> : null}
           <div className="form-view-actions">
