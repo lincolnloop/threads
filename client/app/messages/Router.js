1f0daf566ef4b367a4ab7e63d62fa688b201352e
Marco Louro <mlouro@gmail.com>
2015-10-29 09:54:31 +0100

    Revert "Remove deprecated code"
    This reverts commit 77e3e59d909bb194d2c8466e3771b1bd8815e7b7.

--- client/app/messages/Router.js
+++ client/app/messages/Router.js
@@ -6,6 +6,7 @@ var dispatcher = require('../dispatcher');
 var urls = require('../urls');
 var store = require('../store');
 var MessageEditView = require('./MessageEdit.jsx');
+var MessageReplyView = require('./MessageReply.jsx');
 var MessageForkView = require('./MessageFork.jsx');
 var TeamDiscussions = require('../discussions/TeamDiscussions.jsx');
 
@@ -34,6 +35,35 @@ var MessageRouter = Router.extend({
     return dispatcher.small({
       'navLevel': 20,
       'title': 'Edit message',
+      'back': this.getBackUrl(),
+      'main': mainView
+    }).medium({
+      'team': team,
+      'main': mainView
+    }).large({
+      'team': team,
+      'discussion': discussion,
+      'list': React.createElement(TeamDiscussions, {
+        'team': team,
+        'key': teamSlug,
+        'discussionId': discussionId
+      }),
+      'main': mainView
+    }).render();
+  },
+
+  reply: function(teamSlug, discussionId, discussionSlug, messageId) {
+    var mainView = React.createElement(MessageReplyView, {
+      'parent_url': urls.get('api:messageChange', {'message_id': messageId})
+    });
+    var team = store.find('teams', {'slug': teamSlug});
+    var discussionUrl = urls.get('api:discussionChange', {
+      'discussion_id': discussionId
+    });
+    var discussion = store.find('discussions', discussionUrl);
+    return dispatcher.small({
+      'navLevel': 20,
+      'title': 'Reply to message',
       'back': this.getBackUrl(),
       'main': mainView
     }).medium({
